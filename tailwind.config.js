025bcf26a08ddbd1416fe3ebac175bb015f327e1
ktscates <cates2506@gmail.com>
2024-07-09 16:16:16 +0200

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#EFF5FF",
+        borderColor: "#D6D9E6",
+        denim: "#022959",
+        grey: "#9699AA",
+        lightBlue: "#ABBCFF",
+        lightGrey: "#D6D9E6",
+        orange: "#FFAF7E",
+        pink: "#F9818E",
+        purple: "#483EFF",
+        redError: "#EE374A",
+        skyBlue: "#BEE2FD",
+        veryLightGrey: "#F8F9FF",
+      },
+
+      fontFamily: {
+        primary: ["Ubuntu", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
