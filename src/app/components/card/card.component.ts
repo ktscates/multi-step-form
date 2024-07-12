ec98d558586ce1b77a0103190025eb79abd6f92c
ktscates <cates2506@gmail.com>
2024-07-12 10:53:10 +0200

    personal info forms

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SideComponent } from '../side/side.component';
 import { ButtonComponent } from '../button/button.component';
+import { InputComponent } from '../input/input.component';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [SideComponent, ButtonComponent],
+  imports: [SideComponent, InputComponent, ButtonComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css',
 })
