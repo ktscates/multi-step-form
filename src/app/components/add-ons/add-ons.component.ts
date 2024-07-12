0cf7886cc6838d547726c6794f0efcb14d94188a
ktscates <cates2506@gmail.com>
2024-07-12 14:22:17 +0200

    add-ons component

---
+++ src/app/components/add-ons/add-ons.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-ons',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-ons.component.html',
+  styleUrl: './add-ons.component.css'
+})
+export class AddOnsComponent {
+
+}
