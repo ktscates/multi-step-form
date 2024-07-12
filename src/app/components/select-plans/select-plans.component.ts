d9fef6f786368354b1ed2a7909ab062327fc2fd6
ktscates <cates2506@gmail.com>
2024-07-12 12:53:43 +0200

    select plans component

---
+++ src/app/components/select-plans/select-plans.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-select-plans',
+  standalone: true,
+  imports: [],
+  templateUrl: './select-plans.component.html',
+  styleUrl: './select-plans.component.css'
+})
+export class SelectPlansComponent {
+
+}
